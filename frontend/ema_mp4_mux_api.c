56ff23a3cc71ce6db54e94b3960503df68439d37
Aleksandr Sokolovskii <amsokol+github@gmail.com>
2022-07-10 12:45:08 +0300

    added muxing progress logging
    Signed-off-by: Aleksandr Sokolovskii <amsokol+github@gmail.com>

--- frontend/ema_mp4_mux_api.c
+++ frontend/ema_mp4_mux_api.c
@@ -481,6 +481,8 @@ ema_mp4_mux_start(ema_mp4_ctrl_handle_t handle)
         CHK_ERR_RET(ret);
     }
     
+    int steps = handle->usr_cfg_mux.es_num + 1;
+
     /**** write all other boxes */
 
     /** parsing all ES source */
@@ -488,6 +490,8 @@ ema_mp4_mux_start(ema_mp4_ctrl_handle_t handle)
     {
         usr_cfg_es = &(handle->usr_cfg_ess[es_idx]);
         handle->mp4_handle->curr_usr_cfg_stream_index = es_idx;
+
+        printf("[%d/%d] Parsing track \"%s\"... ", es_idx + 1, steps, usr_cfg_es->input_fn);
 
         /** check if track to add, delete, replace does even exist */
         if (usr_cfg_es->mp4_tid > handle->usr_cfg_mux.es_num)
@@ -572,7 +576,11 @@ ema_mp4_mux_start(ema_mp4_ctrl_handle_t handle)
                 CHK_ERR_RET(ret);
             }
         }
+
+        printf("Done\n");
     }
+
+    printf("[%d/%d] Saving output file(s) \"%s\"... ", steps, steps, handle->usr_cfg_mux.output_fn);
 
     /**** the summary part of mp4 file is ready and output */
     msglog(NULL, MSGLOG_INFO, "Output headers\n");
@@ -582,6 +590,8 @@ ema_mp4_mux_start(ema_mp4_ctrl_handle_t handle)
     msglog(NULL, MSGLOG_INFO, "\nOutput tracks\n");
     ret = mp4_muxer_output_tracks(handle->mp4_handle);
     CHK_ERR_RET(ret);
+
+    printf("Done\n");
 
     msglog(NULL,MSGLOG_INFO,"\n");
     return EMA_MP4_MUXED_OK;
